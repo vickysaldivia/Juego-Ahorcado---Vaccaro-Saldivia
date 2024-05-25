d113ed103487785807998f09ebbd0ec4acd0043b
Giuseppe Vaccaro <gvaccaro@correo.unimet.edu.ve>
2024-05-25 18:29:05 -0400

    merge de ambos códigos
    Ahora ya podemos empezar a trabajar la parte del javaScript de cada uno de los archivos

---
+++ scripts/scriptIndex.js
@@ -0,0 +1,37 @@
+function mostrarFondoOscuro() {
+    const fondoOscuro = document.getElementById("fondoOscuro")
+    fondoOscuro.style.display = "block";
+}
+function mostrarInstruccion() {
+    const ventanaContenedor = document.getElementById("ventanaContenedor");
+    ventanaContenedor.style.display = "block";
+}
+
+function ocultar() {
+    const ocultarVentana = document.getElementById("ventanaContenedor");
+    ocultarVentana.style.display = "none";
+    const fondoOscuro = document.getElementById("fondoOscuro")
+    fondoOscuro.style.display = "none";
+}
+
+document
+  .getElementById("datosJugador")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var jugadorInput = document.getElementById("jugador").value;
+    // console.log(jugadorInput);
+    var jugador = { nombsre: jugadorInput, puntos: 0};
+    sessionStorage.setItem("jugador", JSON.stringify(jugador));
+
+      window.location.href = "juego.html";
+});
+
+function formularioJugador() {
+    const datos = document.getElementById("datosJugador")
+    datos.style.display = "block";
+}
+function mostrarInstruccion2() {
+    const ventanaContenedor = document.getElementById("ventanaContenedor2");
+    ventanaContenedor.style.display = "block";
+}
+
