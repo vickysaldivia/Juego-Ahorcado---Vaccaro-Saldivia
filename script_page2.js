ffcc72bcaa9bbe3672035635e0fe5f5b1024a39c
Giuseppe Vaccaro <gvaccaro@correo.unimet.edu.ve>
2024-05-25 20:51:43 -0400

    Actualización del branch

--- script_page2.js
+++
@@ -1,9 +0,0 @@
-function mostrarFondoOscuro() {
-    var fondoOscuro = document.getElementById("fondoOscuro");
-    fondoOscuro.style.display = "block";
-}
-
-function ocultarFondoOscuro() {
-    var fondoOscuro = document.getElementById("fondoOscuro");
-    fondoOscuro.style.display = "none";
-}
